7ffc83dd998cce92bc51996a37df949b663c4152
yzsvdu <yzsvdu@gmail.com>
2024-01-05 21:23:18 -0800

    fixed ssl

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -8,7 +8,7 @@ fi
 domains=(qiaoqiao.online www.qiaoqiao.online)
 rsa_key_size=4096
 data_path="./docker/nginx/certbot"
-email="" # Adding a valid address is strongly recommended
+email="yzsvdu@gmial.com" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
